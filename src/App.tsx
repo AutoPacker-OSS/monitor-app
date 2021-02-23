fe5618934cf927a19cb05da774210871ccc7293f
Aron Nicholasson <aronmar@live.no>
2021-02-23 18:54:39 +0100

    Add formatter

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<div>Content goes here :)</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div>Content goes here :)</div>
+    </>
+  );
 }
 
 export default App;
